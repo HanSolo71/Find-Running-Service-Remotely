a2d95693ba3f4d2229af7ff084c5ae1e4cabe53b
Austin Downing <HanSolo552@gmail.com>
2018-10-29 10:54:10 -0400

    Add files via upload

---
+++ Find Running Service.ps1
@@ -0,0 +1,10 @@
+﻿#Gets all workstations that need to have software installed, if you don't want to uninstall all of the software from you will need to use a text document and Get-Content
+$Machines = Get-ADComputer -Filter * -SearchBase "OU=Workstations,DC=Contoso,DC=COM" | Select DNSHostName -ExpandProperty DNSHostname
+
+#Name of service to determine if it is running
+$ServiceName = "Service"
+
+foreach ($Machine in $Machines) {
+Write-Output "`r`n The Machine being evaluated is $Machine"
+tasklist /S $Machine /FI "SERVICES eq $ServiceName"
+}
